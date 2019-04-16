9b7bd195a274185e7703a7337561d43f877c0f40
Nguyễn Quốc Anh - CMC Global DU1 <nqanh4@LDAP.CMCGLOBAL.COM.VN>
2019-04-16 13:47:35 +0700

    init

---
+++ src/main/java/com/example/demo/dto/ApiError.java
@@ -0,0 +1,29 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import com.example.demo.exception.bookexception.BusinessErrorCode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+	String message;
+	List<String> details;
+	BusinessErrorCode businessQueryErrorCode;
+	HttpStatus httpStatus;
+
+	public ApiError(String message, List<String> details) {
+		super();
+		this.message = message;
+		this.details = details;
+	}
+}
