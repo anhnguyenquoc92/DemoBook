8d8dcb73dfcef7701b7112c8c662dcf121742554
anhnguyenquoc92 <nguyenquocanh01061992@gmail.com>
2020-05-03 14:52:32 +0700

    .

--- src/main/java/com/example/demo/utility/ValidParam.java
+++ src/main/java/com/example/demo/utility/ValidParam.java
@@ -7,9 +7,15 @@ import com.example.demo.exception.bookexception.DuplicateCodeException;
 import com.example.demo.exception.bookexception.ValidParamException;
 
 public class ValidParam {
-	public static void checkUniqueCode(List<String> codes, String code) {
+	public static void checkUniqueCode(List<String> codes, String code, List<Integer> bookIds, int bookId) {
 		System.out.println("test merge 1");
 		System.out.println("test merge 2");
+		if (bookIds.contains(bookId)) {
+			throw new DuplicateBookIdException("BookId should be unique");
+		}
+		if (bookId < 1) {
+			throw new ValidParamException("Book id should be greater than 1");
+		}
 	}
 
 	public static void checkUniqueBookId(List<Integer> bookIds, int bookId) {
